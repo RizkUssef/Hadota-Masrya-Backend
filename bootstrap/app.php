f968cb51aa3489523e15a2c9a838ef8472756916
RizkUssef <rizkusgt67@gmail.com>
2026-04-12 20:09:05 +0200

    add(sanctum):set the sanctum configrations to handle the api using the SPA mode in it

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->statefulapi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
