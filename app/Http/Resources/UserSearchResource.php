7ef4b4d53bcf0b838dc8fbe33d34ab6ad601a856
RizkUssef <rizkusgt67@gmail.com>
2026-04-14 23:40:06 +0200

    add(UserResource):handel the data that i need to return in the resource

--- app/Http/Resources/UserSearchResource.php
+++ app/Http/Resources/UserSearchResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class UserSearchResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,19 +15,13 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'uuid' => $this->uuid,
             'username' => $this->username,
-            'email' => $this->email,
-            'phone' => $this->phone,
             'display_name' => $this->display_name,
             'avatar_url' => $this->avatar_url,
             'bio' => $this->bio,
-            'status' => $this->status,
-            'is_online' => $this->is_online,
-            'last_seen_at' => $this->last_seen_at,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at,
+            "is_online" => $this->is_online,
+            "status" => $this->status,
         ];
     }
 }
